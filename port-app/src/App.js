632c51e729a52b72c262d661a044543e8a2d1ebd
oishbdh <ikechukwuarinze614@gmail.com>
2023-05-20 20:28:05 +0100

    port

--- port-app/src/App.js
+++ port-app/src/App.js
@@ -1,18 +1,9 @@
-import github from "./assets/github.png";
 import "./App.css";
-import "./Main.css";
 
-import Main from "./components/Main";
-
-function Port(props) {
-  const port = <"img src={github}" />;
-  return port;
-}
 function App() {
   return (
     <div className="App">
-      <Main />;
-      <Port />;
+      
     </div>
   );
 }
