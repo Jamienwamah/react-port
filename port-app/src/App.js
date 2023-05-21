be285bb5d66928c5a7308a7e1a78586d76d9d902
oishbdh <ikechukwuarinze614@gmail.com>
2023-05-21 09:53:42 +0100

    my_port

--- port-app/src/App.js
+++ port-app/src/App.js
@@ -1,9 +1,11 @@
+import Header from './Components/header/Header';
+import Nav from './Components/nav/Nav';
 import "./App.css";
-
 function App() {
   return (
     <div className="App">
-      
+      <Header />
+      <Nav/>
     </div>
   );
 }
---
+++ port-app/src/Components/header/Header.js
@@ -0,0 +1,16 @@
+import './Header.css';
+
+function Header() {
+    return (
+        <div id="home" className="container header-container">
+            <h3>
+                <p>
+                    I <span>'</span>m
+                </p>
+                Ikechukwu
+                <h2>Software Developer</h2>
+            </h3>
+        </div>
+    );
+};
+export default Header;
