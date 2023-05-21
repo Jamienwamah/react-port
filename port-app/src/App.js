9946e9f029a980154919ee178893756681b554f1
oishbdh <ikechukwuarinze614@gmail.com>
2023-05-21 10:18:32 +0100

    my_port

--- port-app/src/App.js
+++ port-app/src/App.js
@@ -1,11 +1,13 @@
-import Header from './Components/header/Header';
-import Nav from './Components/nav/Nav';
+import Header from "./Components/header/Header";
+import Nav from "./Components/nav/Nav";
+import Home from "./Components/home/Home";
 import "./App.css";
 function App() {
   return (
     <div className="App">
       <Header />
-      <Nav/>
+      <Nav />
+      <Home />
     </div>
   );
 }
---
+++ port-app/src/Components/home/Home.js
@@ -0,0 +1,26 @@
+import './Home.css';
+import logo from '../props/logo.png';
+import {BsMouse} from 'react-icons/bs'
+
+function Home() {
+    return (
+        <div id='home' className="container home-container">
+            <div className="logo">
+                <div className="main-img">
+                    <span className="circle"></span>
+                    <span className="circle"></span>
+                    <span className="circle"></span>
+                    <span className="circle"></span>
+                    <span className="circle"></span>
+                    <span className="circle"></span>
+                    <span className="circle"></span>
+                    <span className="circle"></span>
+                </div>
+            </div>
+
+        </div>
+    );
+};
+
+export default Home;
+
